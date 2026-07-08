3db1f75708bbac07e295716b75cc8098639abe09
Andras Serfozo <subztep@gmail.com>
2026-07-08 06:22:13 +0100

    feat: download dataset

--- README.md
+++ README.md
@@ -1,8 +1,39 @@
-Python 3.11 required.
+## Run locally
 
-```sh
-python train.py
-```
+> [!IMPORTANT]  
+> Python 3.11 required.
+
+1. Create a `.env` file based on [`.env.example`](.env.example), and populate it with your [API key](https://mozilladatacollective.com/profile/credentials).
+
+2. Create a virtual environment:
+   ```sh
+   python -m venv .venv
+   ```
+
+2. Activate it:
+   ```sh
+   source .venv/bin/activate
+   ```
+
+3. Install dependencies
+   ```sh
+   pip install -r requirements.txt
+   ```
+
+4. Install CUDA-specific PyTorch **(optional)**:
+   ```sh
+   pip install torch==2.2.2+cu118 torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+   ```
+
+5. Download dataset:
+   ```sh
+   python download_dataset.py
+   ```
+
+6. Run the project:
+   ```sh
+   python train.py
+   ```
 
 ---
 
---
+++ download_dataset.py
@@ -0,0 +1,47 @@
+from datacollective import download_dataset
+from pathlib import Path
+import tarfile
+
+# DATASET_ID = "cmqinob6900wknr07s6fgcprx"
+# Dataset slug also accepted
+DATASET_ID = "common-voice-scripted-speech-26-0-hungar-22d743b4"
+
+DATA_DIR_RELATIVE = "data"
+ROOT = Path(__file__).parent
+DATA_DIR = ROOT / DATA_DIR_RELATIVE
+
+print(f"Using data directory: {DATA_DIR}")
+
+DATA_DIR.mkdir(exist_ok=True)
+
+if any(DATA_DIR.iterdir()):
+    raise RuntimeError(
+        f"{DATA_DIR} is not empty. "
+        "Please remove existing files before downloading the dataset."
+    )
+
+print("Downloading dataset...")
+
+download_dataset(
+    DATASET_ID,
+    download_directory=DATA_DIR_RELATIVE,
+)
+
+archives = list(DATA_DIR.glob("*.tar.gz"))
+
+if len(archives) != 1:
+    raise RuntimeError(
+        f"Expected exactly one tar.gz archive, found {len(archives)}"
+    )
+
+archive = archives[0]
+
+print(f"Extracting {archive.name}...")
+
+with tarfile.open(archive, "r:gz") as tar:
+    tar.extractall(DATA_DIR)
+
+print("Removing archive...")
+archive.unlink()
+
+print("Dataset ready.")
